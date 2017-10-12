13d0e6b380f3fd949561fb6c6b821b20118b84e5
Black <756319278@qq.com>
2017-10-12 15:14:41 +0800

    修改i18n  可使用iview再带locale  但暂时需要把i18n版本回退到5.0.3

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import router from './router'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';    // 使用 CSS
 
+/**
+ * @author Black_晨
+ * 如要使用一下代码， 请将i18n 版本暂时回退到 5.0.3
+ */
+// import zhLocale from 'iview/dist/locale/zh-CN';
+// import enLocale from 'iview/dist/locale/en-US';
+
+// Vue.config.lang = 'en-US';
+// Vue.locale('zh-CN', zhLocale);
+// Vue.locale('en-US', enLocale);
+
+
 Vue.use(iView)
 Vue.config.productionTip = false
 
